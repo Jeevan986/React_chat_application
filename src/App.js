62988e22175b9729467053bd5ac951a3cd4441ac
Jeevan Bastola <jeevanbastola986@gmail.com>
2021-10-25 18:02:59 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <ChatEngine
+            height ="100vh"
+            projectID="eac609c7-ac19-4e59-8df0-f9efad2c3c6e"
+            userName="jeevanBastola"
+            userSecret="jb123"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+        />
+    );
 }
 
 export default App;
+
+// import React from 'react';
+// import { ChatEngine } from 'react-chat-engine';
+// import './App.css';
+// export function App() {
+// 	return (
+// 		<ChatEngine
+// 			height='100vh'
+// 			userName='jeevanBastola'
+// 			userSecret='jb123'
+// 			projectID='eac609c7-ac19-4e59-8df0-f9efad2c3c6e'
+// 		/>
+// 	);
+// }
